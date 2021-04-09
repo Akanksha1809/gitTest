d1947e0f35beadcc6ee4181d1a2b93b0322a34a0
Gaurav Deora <gauravdeora@rarecrew.com>
2021-04-09 06:29:00 +0000

    Final Commit

---
+++ Shourya/script.js
@@ -0,0 +1,12 @@
+function func1() {
+    document.getElementById('yok').style.backgroundColor = "red";
+}
+
+function func2() {
+    document.getElementById('yok').style.backgroundColor = "black";
+}
+
+function func() {
+    document.getElementById("y").style.backgroundColor = "yellow";
+
+}

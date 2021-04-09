da632154ddae960c2b484b1c869f9c3daafd64b5
Akanksha <akankshaakumar@rarecrew.com>
2021-04-09 05:34:31 +0000

    commit

---
+++ dev.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+  function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("mystyle");
+  }
